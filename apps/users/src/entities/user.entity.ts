09826931e6290a76106c4eb41214f8b8da5844f5
RattanakpanhaKong <krattanakpanha@gmail.com>
2024-11-15 16:14:26 +0700

    dto, entities, types

---
+++ apps/users/src/entities/user.entity.ts
@@ -0,0 +1,44 @@
+import { Directive, Field, ObjectType } from '@nestjs/graphql';
+
+@ObjectType()
+@Directive('@key(fields:"id")')
+export class Avatars {
+  @Field()
+  id: string;
+
+  @Field()
+  public_id: string;
+
+  @Field()
+  url: string;
+
+  @Field()
+  user_id: string;
+}
+
+@ObjectType()
+export class User {
+  @Field()
+  user: string;
+
+  @Field()
+  name: string;
+
+  @Field()
+  email: string;
+
+  @Field()
+  password: string;
+
+  @Field(() => Avatars, { nullable: true })
+  avatar?: Avatars | null;
+
+  @Field()
+  role: string
+
+  @Field()
+  createAt: Date
+
+  @Field()
+  updateAt: Date
+}
